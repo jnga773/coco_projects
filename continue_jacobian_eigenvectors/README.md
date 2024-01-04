e364588c9b25240a5c49a721eb99e7f3bf378ee3
Jacob Ngaha <jnga773@aucklanduni.ac.nz>
2024-01-05 11:05:30 +1300

    updated following eigenvectors

--- continue_jacobian_eigenvectors/README.md
+++ continue_jacobian_eigenvectors/README.md
@@ -8,25 +8,25 @@ We set out the example in the following steps:
 
 1. We first compute the eigenvalues and eigenvectors using the in-built `eig` function, and will apply these as the initial solutions to the eigenvector boundary conditions.
 
-2. The boundary conditions are then applied with the `boundary_conditions_eig()` function, added as a monitor function. For the unstable eigenvector:
+2. The boundary conditions are then applied with the `boundary_conditions_eig()` function, added as a monitor function. For example:
    ```MATLAB
-   prob = coco_add_func(prob, 'bcs_eig_unstable', @boundary_conditions_eig, [], ...
+   prob = coco_add_func(prob, 'bcs_eig1', @boundary_conditions_eig, [], ...
                         'zero', 'uidx', ...
                         [INDICES_OF_EQUILIBRIUM_POINT, INDICES_OF_PARAMETERS], ...
-                        'u0', [unstable_vector_in, unstable_value_in]);
+                        'u0', [vec1_in, val1_in]);
    ```
 
 3. The components of the eigenvector and the eigenvalue are then added as active parameters:
    ```MATLAB
    % Read indices from added monitor function
-   uidx_eigu = coco_get_func_data(prob, 'bcs_eig_unstable', 'uidx');
+   uidx_eigu = coco_get_func_data(prob, 'bcs_eig1', 'uidx');
    % Get corresponding indices for eigenvector and eigenvalue
    vu_idx = [numel(uidx_eigu) - 3; numel(uidx_eigu) - 2; numel(uidx_eigu) - 1];
    lu_idx = numel(uidx_eigu);
    % Add as parameters
-   prob = coco_add_pars(prob, 'par_eig_unstable', ...
+   prob = coco_add_pars(prob, 'par_eig1', ...
                         [uidx_eigu(data_out.vu_idx); uidx_eigu(data_out.lu_idx)], ...
-                        {'vu_1', 'vu_2', 'lu'});)
+                        {'vec1_1', 'vec1_2', ..., 'lam1'});)
    ```
 
 4. Repeat steps 2 and 3 for the stable eigenvector and eigenvalue.
--- continue_jacobian_eigenvectors/eigenvalues_eigenvectors_continuation.m
+++ continue_jacobian_eigenvectors/eigenvalues_eigenvectors_continuation.m
@@ -50,20 +50,13 @@ J_dFdX = func_DFDX(x0, p0);
 % Calculate unstable eigenvectors and eigenvalues
 [eigvec, eigval] = eig(J_dFdX);
 
-% Indices for unstable eigenvalue
-unstable_index = diag(eigval) > 0;
-% Indices for stable eigenvalue
-stable_index   = diag(eigval) < 0;
+% Eigenvectors
+vec1 = eigvec(:, 1);
+vec2 = eigvec(:, 2);
 
-% Unstable eigenvector
-vu = eigvec(:, unstable_index);
-% Stable eigenvector
-vs = eigvec(:, stable_index);
-
-% Unstable eigenvalue
-lu = eigval(unstable_index, unstable_index);
-% Stable eigenvalue
-ls = eigval(stable_index, stable_index);
+% Eigenvalues
+lam1 = eigval(1, 1);
+lam2 = eigval(2, 2);
 
 %----------------------------%
 %     Setup COCO Problem     %
@@ -80,52 +73,52 @@ prob = ode_isol2ep(prob, 'x0', func_list{:}, x0, pnames, p0);
 % Extract indices
 maps = data.ep_eqn;
 
-%---------------------------------------%
-%     Boundary Conditions: Unstable     %
-%---------------------------------------%
+%--------------------------------------%
+%     Boundary Conditions: Eigen-1     %
+%--------------------------------------%
 % Append boundary conditions to continue the unstable eigenvalues and eigenvectors
-prob = coco_add_func(prob, 'bcs_eig_unstable', @boundary_conditions_eig, [], ...
+prob = coco_add_func(prob, 'bcs_eig1', @boundary_conditions_eig, [], ...
                      'zero', 'uidx', ...
                      [uidx(maps.x_idx); ...
                       uidx(maps.p_idx)], ...
-                     'u0', [vu; lu]);
+                     'u0', [vec1; lam1]);
 
 % Get u-vector indices from this coco_add_func call, including the extra
 % indices from the added "vu" and "lu".
-uidx_eigu = coco_get_func_data(prob, 'bcs_eig_unstable', 'uidx');
+uidx_eigu = coco_get_func_data(prob, 'bcs_eig1', 'uidx');
 
 % Grab eigenvector and value indices from u-vector
 vu_idx = [numel(uidx_eigu) - 2; numel(uidx_eigu) - 1];
 lu_idx = numel(uidx_eigu);
 
 % Define active parameters for unstable eigenvector and eigenvalue
-prob = coco_add_pars(prob, 'eig_unstable', ...
+prob = coco_add_pars(prob, 'par_eig1', ...
                      [uidx_eigu(vu_idx); uidx_eigu(lu_idx)], ...
-                     {'vu_1', 'vu_2', 'lu'}, ...
+                     {'vec1_1', 'vec1_2', 'lam1'}, ...
                      'active');
 
-%-------------------------------------%
-%     Boundary Conditions: Stable     %
-%-------------------------------------%
+%--------------------------------------%
+%     Boundary Conditions: Eigen-1     %
+%--------------------------------------%
 % Append boundary conditions to continue the unstable eigenvalues and eigenvectors
-prob = coco_add_func(prob, 'bcs_eig_stable', @boundary_conditions_eig, [], ...
+prob = coco_add_func(prob, 'bcs_eig2', @boundary_conditions_eig, [], ...
                      'zero', 'uidx', ...
                      [uidx(maps.x_idx); ...
                       uidx(maps.p_idx)], ...
-                     'u0', [vs; ls]);
+                     'u0', [vec2; lam2]);
 
 % Get u-vector indices from this coco_add_func call, including the extra
 % indices from the added "vu" and "lu".
-uidx_eigs = coco_get_func_data(prob, 'bcs_eig_stable', 'uidx');
+uidx_eigs = coco_get_func_data(prob, 'bcs_eig2', 'uidx');
 
 % Grab eigenvector and value indices from u-vector
 vs_idx = [numel(uidx_eigs) - 2; numel(uidx_eigs) - 1];
 ls_idx = numel(uidx_eigs);
 
 % Define active parameters for unstable eigenvector and eigenvalue
-prob = coco_add_pars(prob, 'eig_stable', ...
+prob = coco_add_pars(prob, 'par_eig2', ...
                      [uidx_eigs(vs_idx); uidx_eigs(ls_idx)], ...
-                     {'vs_1', 'vs_2', 'ls'}, ...
+                     {'vec2_1', 'vec2_2', 'lam2'}, ...
                      'active');
 
 %--------------------------------%
